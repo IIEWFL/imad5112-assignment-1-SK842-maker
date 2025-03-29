621969586df8e18ad9545856857ccd9233fbe42b
samke <samkelokhoza2006@gmail.com>
2025-03-29 17:50:28 +0200

    First commit

--- app/src/main/java/com/example/mealsuggestorapp/MainActivity.kt
+++ app/src/main/java/com/example/mealsuggestorapp/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
 import kotlin.system.exitProcess
 
 
